a79a70d3d8f6b03c165fbbb2fe03b4ac413fb588
Angelina Grein <angelina.grein@non.schneider-electric.com>
2018-01-10 17:26:46 +0100

    add seperate Class for https request and adapt code in DetailsActivity

---
+++ app/src/main/java/hs_ab/com/TrinkBar/Bar.java
@@ -0,0 +1,87 @@
+package hs_ab.com.TrinkBar;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Bar {
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("picture")
+    @Expose
+    private String picture;
+    @SerializedName("coordinates")
+    @Expose
+    private List<Coordinate> coordinates = null;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("image_link")
+    @Expose
+    private String imageLink;
+    @SerializedName("opening_hours")
+    @Expose
+    private String openingHours;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public List<Coordinate> getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(List<Coordinate> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageLink() {
+        return imageLink;
+    }
+
+    public void setImageLink(String imageLink) {
+        this.imageLink = imageLink;
+    }
+
+    public String getOpeningHours() {
+        return openingHours;
+    }
+
+    public void setOpeningHours(String openingHours) {
+        this.openingHours = openingHours;
+    }
+
+}
